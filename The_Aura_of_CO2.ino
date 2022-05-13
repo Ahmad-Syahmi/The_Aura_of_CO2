aad3da290a2ce5032baeec172f5c2bc359981f73
Ahmad Syahmi <imhays287@gmail.com>
2022-05-13 15:16:09 +0800

    Update The_Aura_of_CO2.ino

--- The_Aura_of_CO2.ino
+++ The_Aura_of_CO2.ino
@@ -4,41 +4,43 @@
 #include <DHT.h>
 #include <MQ135.h>
 
-#define gas135 A0
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define DHTPIN 8
-#define OLED_RESET  -1
-#define SCREEN_ADDRESS 0x3C 
+#define gas135 A0 // Define MQ135 Analog Sensor Pin
+#define SCREEN_WIDTH 128 // Define width of OLED display
+#define SCREEN_HEIGHT 64 // Define height of OLED display
+#define DHTPIN 8 // Define DHT22 Digital Sensor Pin
+#define OLED_RESET  -1 // If your OLED Display does not use an onboard reset pin, -1 allows you to share the reset pin with the arduino microcontroller
+#define SCREEN_ADDRESS 0x3C // The I2C address of the OLED display
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
-MQ135 gasSensor(gas135, 145); //RZero is 145
-
-const int buzzer = 3;
+MQ135 gasSensor(gas135, 145); // My calculated Resistance Zero value is 145, you should calibrate the value yourself with your sensor as our atmosphere is likely different.
+                              
+const int buzzer = 3; // Define pin for buzzer and red LED light as alarm
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(buzzer, OUTPUT);
+  pinMode(buzzer, OUTPUT); // Declare buzzer pin mode
   
-  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
-  display.clearDisplay();
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS); // Begin display
+  display.clearDisplay(); // Clear garbage on display
   display.setTextColor(SSD1306_WHITE);
 
-  dht.begin();
+  dht.begin(); // Begint the DHT22 sensor
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int temp = dht.readTemperature();
-  int humy = dht.readHumidity();
+  int temp = dht.readTemperature(); // Read temperature
+  int humy = dht.readHumidity(); // Read humidity
   
-  int correctedPPM = gasSensor.getCorrectedPPM(temp, humy);
+  int correctedPPM = gasSensor.getCorrectedPPM(temp, humy); // Read CO2 PPM
 
-  display.clearDisplay();
+  display.clearDisplay(); // Clear garbage on display again from end of loop
+  
+  // Design and print display
   
   display.setTextSize(1);
   display.setCursor(0,7);
@@ -69,7 +71,7 @@ void loop() {
   
   display.display();
 
-  if (correctedPPM >= 1000)
+  if (correctedPPM >= 1000) // If CO2 PPM exceeds 1000, trigger Alarm
   {
     digitalWrite(buzzer, HIGH);
   }
@@ -78,5 +80,5 @@ void loop() {
     digitalWrite(buzzer, LOW);
   }
   
-  delay(1000);
+  delay(5000); // Loop and read sensors 5 every seconds
 }
